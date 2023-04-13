48339f01ac85194ca58a4ce7d828b96719078163
pvsamuel <psamuel@terpmail.umd.edu>
2023-04-13 17:40:44 -0400

    location added

--- final_project.py
+++ final_project.py
@@ -4,9 +4,11 @@ print(df)
 
 class Apartment:
     
-    def __init__(self, min_budget, num_rooms, location, amenities, floorplan):
+    def __init__(self, min_budget, num_rooms, apt_names, location, amenities, floorplan):
+        
         self.min_budget = {"Terrapin Row":1250, "University View":1200, "The Varsity":1104}
         self.num_rooms = num_rooms
+        self.apt_names = ["Terrapin Row","University View","The Varsity"]
         self.location = location 
         self.amenities = amenities
         self.floorplan = floorplan
@@ -25,12 +27,20 @@ class Apartment:
             return f'You meet the minimum budget of The Varsity: {self.min_budget["The Varsity"]}'
 
     def userInput(self):
-        ideal_apartments = []
+        apt_location = ["North","South"]
+        #apt_some_location = ["Terrapin Row is North","University View is South","The Varsity is South"]
+        
         print("Please answer the following questions to help provide you with your ideal apartment")
         user_name = input("Please enter your full name:")
-        ideal_location = input("Would North campus or South Campus be ideal for you: ")
-        if ideal_location == self.location:
-            return("apple")
+        user_location = input("Which part of UMD campus would be ideal for you. \
+                               Type North or South: ")
+        
+        correct_user_location = [x for x in apt_location if user_location in x]  
+        #This list comprehension checks if the user properly wrote "North" or 
+        # "South". 
+            
+        apt_some_location = ["Terrapin Row is South","University View is North","The Varsity is North"]
+    
             
         
    
