af5897eb3cce67851146cdbce0e1456d2c08f9c9
ShishirPoreddy <shishirporeddy321@gmail.com>
2023-04-14 11:08:13 -0400

    Fixed

--- final_project.py
+++ final_project.py
@@ -30,7 +30,9 @@ class Apartment:
     def userBudget(self,user_input_budget):
 
         user_input_budget = int(input("What is your minimum budget?")) 
-        if user_input_budget < self.min_budget["The Varsity"]:
+        cheapest_apt=min(self.min_budget.values)
+        cheapest_to_expensive = [key for key in self.min_budget if self.min_budget[key]==cheapest_apt]
+        if cheapest_apt:
             raise ValueError("Your budget does not meet the minimum budget for any of the apartments")
         elif user_input_budget >= self.min_budget["Terrapin Row"]:
             return f'You meet the minimum budget of Terrapin Row: {self.min_budget["Terrapin Row"]}'
@@ -81,28 +83,28 @@ class Apartment:
     #Find a more efficent way to do this by traversing the merged csv file.
 
     def check_eligibility(identity_proof, income_proof, residency_proof, insurance_proof):
-    """
-    Check if user meets all the proper documentation for leasing.
+        """
+        Check if user meets all the proper documentation for leasing.
 
-    Args:
-    - identity_proof: string, proof of identity (e.g. driver's license, passport)
-    - income_proof: string, proof of income (e.g. pay stub, bank statement)
-    - residency_proof: string, proof of current residency (e.g. utility bill, lease agreement)
-    - insurance_proof: string, proof of insurance (e.g. auto insurance, renters insurance)
+        Args:
+        - identity_proof: string, proof of identity (e.g. driver's license, passport)
+        - income_proof: string, proof of income (e.g. pay stub, bank statement)
+        - residency_proof: string, proof of current residency (e.g. utility bill, lease agreement)
+        - insurance_proof: string, proof of insurance (e.g. auto insurance, renters insurance)
 
-    Returns:
-    - eligible: boolean, True if user meets all the proper documentation, False otherwise
-    """
+        Returns:
+        - eligible: boolean, True if user meets all the proper documentation, False otherwise
+        """
     # Check if all proofs of documentation are provided
-    if identity_proof is None or income_proof is None or residency_proof is None or insurance_proof is None:
-        print("Please provide all the required documentation.")
-        return False
-    
-    # Check if the user meets the minimum income requirement
-    min_income_requirement = 30000  # set a minimum income requirement of $30,000
-    if income_proof < min_income_requirement:
-        print("Your income does not meet the minimum requirement.")
-        return False
+        if identity_proof is None or income_proof is None or residency_proof is None or insurance_proof is None:
+            print("Please provide all the required documentation.")
+            return False
+        
+        # Check if the user meets the minimum income requirement
+        min_income_requirement = 30000  # set a minimum income requirement of $30,000
+        if income_proof < min_income_requirement:
+            print("Your income does not meet the minimum requirement.")
+            return False
     
     # Check if the residency proof is current
     # You could implement this check by comparing the date on the residency_proof to today's date
@@ -111,12 +113,18 @@ class Apartment:
     # You could implement this check by verifying that the insurance policy is currently active
     
     # If all checks pass, the user is eligible
-    print("Congratulations, you are eligible to lease!")
-    return True
+        print("Congratulations, you are eligible to lease!")
+        return True
         
 
-#def main()
-# Run all the methods here
+def main():
+    apt = Apartment("CP Apartments_Version2.csv", "Amenities.csv", None, None, None, None, None, None)
+    apt.userInput()
+    
+if __name__=='__main__':
+    main()
+    
+
             
         
 
