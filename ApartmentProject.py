8a99231ab545d00996ad1a9323c466a429d83b58
jayyking <jhemel56@terpmail.umd.edu>
2023-05-11 21:50:52 -0400

    fit code within 80- character line

--- ApartmentProject.py
+++ ApartmentProject.py
@@ -170,102 +170,73 @@ class Apartment:
         #Member who worked on this method: Jhemel
         #Technique used: Conditional Expression
         
-        
-        
-
-        
         if proof_of_identity == "yes" and proof_of_income == "yes":
-            print(f"{some_name}, you are eligible to lease an apartment in College Park.")
+            print(f"{some_name}, you are eligible to lease an apartment in "
+            "College Park.")
             self.proof_of_identity_boolean = True
             self.proof_of_income_boolean = True
 
         elif proof_of_identity == "yes" and proof_of_income == "no":
-            print(f"{some_name}, you are not eligible to lease an apartment in College Park." 
+            print(f"{some_name}, you are not eligible to lease an apartment "
+            "in College Park." 
             "\nPlease provide proof of income to proceed further.")
             self.proof_of_identity_boolean = True
             self.proof_of_income_boolean = False
         elif proof_of_identity == "no" and proof_of_income == "yes":
-            print(f"{some_name}, you are not eligible to lease an apartment in College Park." 
+            print(f"{some_name}, you are not eligible to lease an apartment "
+            "in College Park." 
             "\nPlease provide proof of identity to proceed further.")
             self.proof_of_identity_boolean = False
             self.proof_of_income_boolean = True
         else:
-            return(f"{some_name}, you are not eligible to lease an apartment in College Park.")
+            return(f"{some_name}, you are not eligible to lease an apartment "
+            "in College Park.")
         
-        result = "Identity and income verified." if proof_of_identity == True and proof_of_income == True else "Identity and/or income not verified"
+        result = "Identity and income verified." if (proof_of_identity == True 
+        and proof_of_income == True) else "Identity and/or income not verified"
         print(result)
 
-        # Will be used in the future:
-            # Check if the user meets the minimum income requirement
-            #min_income_requirement = 30000  # set a minimum income requirement of $30,000
-            #if income_proof < min_income_requirement:
-                
-                #print("Your income does not meet the minimum requirement.")
-                #return False
-            
-            # Check if the residency proof is current
-            # You could implement this check by comparing the date on the residency_proof to today's date
-            
-            # Check if the insurance proof is valid
-            # You could implement this check by verifying that the insurance policy is currently active
-            
-            # If all checks pass, the user is eligible
-            #print("Congratulations, you are eligible to lease!")
-            #return True    
-                
-        #Prof's advice: 
-        #  Make the method more generic 
-        #  return True if eligible or False if not eligible
-            
         
-        # Will be used in the future:
-            # Check if the user meets the minimum income requirement
-            #min_income_requirement = 30000  # set a minimum income requirement of $30,000
-            #if income_proof < min_income_requirement:
-                
-                #print("Your income does not meet the minimum requirement.")
-                #return False
-            
-            # Check if the residency proof is current
-            # You could implement this check by comparing the date on the residency_proof to today's date
-            
-            # Check if the insurance proof is valid
-            # You could implement this check by verifying that the insurance policy is currently active
-            
-            # If all checks pass, the user is eligible
-            #print("Congratulations, you are eligible to lease!")
-            #return True
     
     def find_shared_group_apartment(self, num_people, some_apartment, budget):
         """
-        Finds apartments that can accommodate a group of people with the given criteria.
+        Finds apartments that can accommodate a group of people with the given 
+        criteria.
 
         Args:
-        - num_people (int): The number of people who want to live together in the apartment.
+        - num_people (int): The number of people who want to live together 
+        in the apartment.
         - some_apartment (str): The apartment chosen by the user.
-        - budget (float): The monthly rent per person in the group for the chosen apartment.
+        - budget (float): The monthly rent per person in the group for 
+        the chosen apartment.
         Returns:
         - A list of apartments that meet the given criteria.
         """
     # #Techniques used: f-strings containing expressions
     # #Member who worked on this method: Jhemel
     # Assume we have a list of available apartments with their details
-        chosen_apartment_df =  self.apartments_df[ self.apartments_df['Apartment Name'] == self.chosen_apartment]
+        chosen_apartment_df =  self.apartments_df[
+            self.apartments_df['Apartment Name'] == self.chosen_apartment]
         #print(chosen_apartment_df)
-        chosen_rooms_available_df = chosen_apartment_df[chosen_apartment_df['Number of Rooms Available']>= self.num_people ]
+        chosen_rooms_available_df = chosen_apartment_df[
+            chosen_apartment_df['Number of Rooms Available']>= self.num_people ]
         print("\nThese are the rooms available that can fit the number of tenants:")
         print(chosen_rooms_available_df)
 
-        chosen_apartment_number = int(input("\nWhich of the apartment units listed best fit your needs?"
-        "\nPlease specify the Apartment Number of the unit shown under the Apartment Number column (e.g. 500):" ))
-        apartment_number_df = chosen_apartment_df[chosen_apartment_df['Apartment Number']== chosen_apartment_number]
+        chosen_apartment_number = int(input("\nWhich of the apartment units "
+        "listed best fit your needs?"
+        "\nPlease specify the Apartment Number of the unit shown under "
+        "the Apartment Number column (e.g. 500):" ))
+        apartment_number_df = chosen_apartment_df[
+            chosen_apartment_df['Apartment Number']== chosen_apartment_number]
         
         #f-strings containing expressions
         print(f"\n{self.user_name}, this is the apartment unit's information:")
         print(apartment_number_df)
         
         #f-strings containing expressions
-        print(f"This is the monthly rent that each of the tenants have to pay (including you): ${budget}.")
+        print("This is the monthly rent that each of the tenants "
+        f"have to pay (including you): ${budget}.")
         
         
     def amenities_rsvp(self, some_apartment, some_amenity='Study Rooms'):
